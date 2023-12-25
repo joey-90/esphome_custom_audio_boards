4325d490d5fee4fd790fb4d16632351fb09d17ec
joey-90 <79873908+joey-90@users.noreply.github.com>
2023-12-25 12:34:35 +0000

    Update board_pins_config.c

--- esp32_s3_korvo1/board_pins_config.c
+++ esp32_s3_korvo1/board_pins_config.c
@@ -40,6 +40,7 @@ esp_err_t get_i2c_pins(i2c_port_t port, i2c_config_t *i2c_config)
     if (port == I2C_NUM_0) {
         i2c_config->sda_io_num = GPIO_NUM_1;
         i2c_config->scl_io_num = GPIO_NUM_2;
+        i2c_config->master.clk_speed = 600000;
     } else {
         i2c_config->sda_io_num = -1;
         i2c_config->scl_io_num = -1;
