ad26cfdd3621a516a1bbc6e7552ace6c1904160d
joey-90 <79873908+joey-90@users.noreply.github.com>
2024-06-17 17:20:41 +0100

    Create __init__.py

---
+++ components/__init__.py
@@ -0,0 +1,14 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+esp_korvo_volume_ns = cg.esphome_ns.namespace('esp_korvo_volume')
+ESPKorvoVolume = esp_box_volume_ns.class_('ESPKorvoVolume', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ESPKorvoVolume),
+}).extend(cv.COMPONENT_SCHEMA)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
