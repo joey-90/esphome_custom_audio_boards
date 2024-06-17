0bddec2c6aaa16e2787d1156c42fae46f2a5ae6d
joey-90 <79873908+joey-90@users.noreply.github.com>
2024-06-17 17:19:19 +0100

    Create esp_korvo_volume.h

---
+++ components/esp_korvo_volume.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "esphome/core/component.h"
+
+namespace esphome
+{
+  namespace esp_korvo_volume
+  {
+
+    class ESPKorvoVolume : public Component
+    {
+    public:
+      void set_volume(float volume);
+    };
+
+    // expose a service for home assistant to use
+    // class ESPBoxVolumeService : public Service
+    // {
+    // public:
+    //   void set_volume(float volume) { static_cast<ESPBoxVolume *>(this->parent())->set_volume(volume); }
+    // };
+
+  } // namespace esp_box_volume
+} // namespace esphome
